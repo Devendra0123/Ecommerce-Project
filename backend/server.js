679953370b85d44f6d6ab46fd718bb348490b4ab
Admin <devendra13.dc@gmail.com>
2021-11-09 13:36:53 +0545

    my commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
+
+//Handling Uncaught exception
+process.on("uncaughtException",(err)=>{
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught exception`);
+  process.exit(1);
+})
+
+//config
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+//connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+const server = app.listen(process.env.PORT, ()=> {
+  console.log(`server is running on htttp://localhost:${process.env.port}`)
+});
+
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection",err=>{
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(()=>{
+    process.exit(1);
+  })
+})
